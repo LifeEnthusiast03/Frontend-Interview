bee57964ec3fada04d8267662020f27316f02498
SOUGATA SAHA <sahasougata820@gmail.com>
2026-01-21 20:55:16 +0530

    ui update

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from "@/components/theme-provider"
 
+ const queryclinet= new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryclinet}>
+         <App />
+      </QueryClientProvider>
+    </ThemeProvider>
+      
   </StrictMode>,
 )
